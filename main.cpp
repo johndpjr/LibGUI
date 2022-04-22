d04b7fdf8a4298a45387a20348a85f963d9bb032
john <johndpowell02@gmail.com>
2022-04-22 12:36:43 -0500

    window abstraction example

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 #include <GLFW/glfw3.h>
+#include "Window.h"
 
 int main() {
     if(!glfwInit()) {
         // Initialization failed
     }
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, World", NULL, NULL);
-    if (!window) {
-        // Window creation failed
-    }
-    while (!glfwWindowShouldClose(window)) {
-        // Keep running
-        // Poll for events
-        glfwPollEvents();
-    }
+    Window window {640, 480, "Hello, World!"};
+    window.mainloop();
     glfwTerminate();
 }
