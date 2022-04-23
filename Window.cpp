1919836026c77e9cce68973e6b0f24b2336364fb
johndpjr <johndpowell02@gmail.com>
2022-04-22 21:56:27 -0500

    Window: add m_root_frame on which all child widgets are placed

--- Window.cpp
+++ Window.cpp
@@ -2,15 +2,12 @@
 
 Window::Window(int width, int height, const std::string& title)
     : m_window{glfwCreateWindow(width, height, title.data(), NULL, NULL)},
+      m_root_frame{std::make_unique<Frame>(new Frame{nullptr})},
       m_title{title}
 {
     if (!m_window) {
         // Window creation failed
     }
-}
-
-Window::Window(Window &parent) {
-
 }
 
 Window::~Window() {
--- Window.h
+++ Window.h
@@ -1,17 +1,21 @@
 #ifndef LIBGUI_WINDOW_H
 #define LIBGUI_WINDOW_H
 
-#include <string>
 #include <GLFW/glfw3.h>
+#include <memory>
+#include <string>
+
+#include "Frame.h"
+
 
 class Window {
 public:
     Window(int width, int height, const std::string& title);
-    Window(Window &parent);
     ~Window();
 
 private:
     GLFWwindow* m_window;
+    std::unique_ptr<Frame> m_root_frame;
     std::string m_title;
 
 public:
